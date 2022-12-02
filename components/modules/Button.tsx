c001648f8270cb540b0eed4f238471aa5fe33433
Dhananjay-JSR <ajaysenday@gmail.com>
2022-12-03 02:41:10 +0530

    Added Hover ani for better ux

--- components/modules/Button.tsx
+++ components/modules/Button.tsx
@@ -4,7 +4,7 @@ type BtnProps = Omit<React.ButtonHTMLAttributes<HTMLButtonElement>,'children'> &
 }
 
 export default function Button({children,variant,...rest}:BtnProps){
-    return <button {...rest} className={`w-28 h-12 font-semibold rounded-md text-xl m-2 ${variant=='dark'? 'bg-[#6f66ef] text-[#eae7fa]' : 'border-[#d4d2ff] border-2 border-solid text-[#6f66ef]'}`}  >
+    return <button {...rest} className={`w-28 h-12 font-semibold active:scale-110 active:transition-all transition-all rounded-md text-xl m-2 ${variant=='dark'? 'bg-[#6f66ef] text-[#eae7fa]' : 'border-[#d4d2ff] border-2 border-solid text-[#6f66ef]'}`}  >
         {children}
     </button>
 }
